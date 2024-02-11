582d98b86e669c84aba23e580f0eb13b25657df2
D-Se <donaldseinen@proton.me>
2024-02-11 14:58:25 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/5li4NY6tBVZpBdXF/scene.splinecode');
